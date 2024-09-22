870077f5eb92b32eab632bfba4cd9cfa1cfaf3e0
Kimhannah <skqkdldhf98@naver.com>
2024-09-22 23:56:48 +0900

    [Feat] MessageCommandUseCase 구현체 생성
    - sendMessage : 채팅방에 메세지 전송
    - deleteMessage: 메세지 삭제

---
+++ src/main/java/com/anchoreer/chat/domain/service/MessageCommandService.java
@@ -0,0 +1,50 @@
+package com.anchoreer.chat.domain.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.anchoreer.chat.application.port.in.MessageCommandUseCase;
+import com.anchoreer.chat.application.port.out.DeleteChatPort;
+import com.anchoreer.chat.application.port.out.LoadChatRoomUserPort;
+import com.anchoreer.chat.application.port.out.SaveChatPort;
+import com.anchoreer.chat.common.exception.custom.ForbiddenException;
+import com.anchoreer.chat.common.exception.custom.NotFoundException;
+import com.anchoreer.chat.domain.entity.Chat;
+import com.anchoreer.chat.domain.entity.ChatRoom;
+import com.anchoreer.chat.domain.entity.ChatRoomUser;
+import com.anchoreer.chat.domain.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MessageCommandService implements MessageCommandUseCase {
+    private final LoadChatRoomUserPort loadChatRoomUserPort;
+    private final SaveChatPort saveChatPort;
+    private final DeleteChatPort deleteChatPort;
+
+    @Override
+    @Transactional
+    public void sendMessage(ChatRoom chatRoom, User sender, String message) {
+        ChatRoomUser chatRoomUser = loadChatRoomUserPort.load(sender, chatRoom)
+            .orElseThrow(() -> new NotFoundException("채팅방에 속해있지 않습니다."));
+
+        Chat chat = Chat.builder()
+            .chatRoomUser(chatRoomUser)
+            .contents(message)
+            .build();
+        saveChatPort.save(chat);
+    }
+
+    @Override
+    @Transactional
+    public void deleteMessage(ChatRoom chatRoom, User sender, Chat chat) {
+        ChatRoomUser chatRoomUser = loadChatRoomUserPort.load(sender, chatRoom)
+            .orElseThrow(() -> new ForbiddenException("채팅방에 속해있지 않습니다."));
+        if (!chat.getChatRoomUser().equals(chatRoomUser)) {
+            throw new ForbiddenException("메시지를 삭제할 수 없습니다.");
+        }
+
+        deleteChatPort.delete(chat);
+    }
+}
